80ae87bea1167eece070aa31131d1fddb1148b81
mengtongun <unmengtong@gmail.com>
2022-04-14 00:01:00 +0700

    Add CardList Component

--- components/body/CardList.tsx
+++ components/body/CardList.tsx
@@ -1,11 +1,16 @@
-import { Card } from '@nextui-org/react';
+import { Grid } from '@nextui-org/react';
+import { StoreCard } from '..';
 
-const CardList = () => {
-  return (
-    <div>
-      <Card />
-    </div>
-  );
-};
-
+const CardList = () => (
+  <Grid.Container gap={2} justify="flex-start">
+    {[...Array(12)].map((_, i) => (
+      <Grid key={i} xs={6} sm={4} md={3} lg={3} xl={2}>
+        <StoreCard />
+      </Grid>
+    ))}
+    <Grid xs={2}>
+      <StoreCard />
+    </Grid>
+  </Grid.Container>
+);
 export default CardList;
