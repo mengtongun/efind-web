c064bde598ebee5b53521f2c666b742a2cad5745
mengtongun <unmengtong@gmail.com>
2022-04-19 20:36:35 +0700

    Update Card List Old

--- components/body/CardList.tsx
+++ components/body/CardList.tsx
@@ -3,14 +3,11 @@ import { StoreCard } from '..';
 
 const CardList = () => (
   <Grid.Container gap={2} justify="flex-start">
-    {[...Array(12)].map((_, i) => (
+    {[...Array(13)].map((_, i) => (
       <Grid key={i} xs={6} sm={4} md={3} lg={3} xl={2}>
         <StoreCard />
       </Grid>
     ))}
-    <Grid xs={2}>
-      <StoreCard />
-    </Grid>
   </Grid.Container>
 );
 export default CardList;
