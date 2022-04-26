e8cdd798f0937fe7ef431a302a497744c7509202
SaTy <mengtong@vtech.one>
2022-04-26 23:47:53 +0700

    Add Supabase to SSR in Index Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,24 @@
 import Body from 'layout/Body';
+import { supabase } from 'libs/supabase';
+import { GetServerSideProps } from 'next';
 
-const IndexPage = () => {
+const IndexPage = (props) => {
+  const { data } = props;
   return (
     <div className="w-full">
-      <Body />
+      <Body stores={data} />
     </div>
   );
 };
 
 export default IndexPage;
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const { data } = await supabase.from('store').select('* ,category:c_id ( name )');
+  console.log(data);
+  return {
+    props: {
+      data,
+    },
+  };
+};
