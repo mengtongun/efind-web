de4cb988ab5e117440926eb00de0d119a5da30d0
mengtongun <unmengtong@gmail.com>
2022-08-11 01:36:34 +0700

    feat: add category context to app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,23 @@ import { NextUIProvider } from '@nextui-org/react';
 import 'antd/dist/antd.css';
 import AuthLayout from 'layout/AuthLayout';
 import MainLayout from 'layout/MainLayout';
+import { useCategory } from 'libs/hooks';
+
 import { useRouter } from 'next/router';
+import { createContext } from 'react';
 import '../styles/globals.css';
+export const CategoryContext = createContext(null);
 
 const MyApp = ({ Component, pageProps }) => {
   const router = useRouter();
-  console.log(router.asPath);
+  const { category } = useCategory();
   return (
     <NextUIProvider>
-      <MainLayout>
-        <Component {...pageProps} />;
-      </MainLayout>
+      <CategoryContext.Provider value={category && category}>
+        <MainLayout>
+          <Component {...pageProps} />
+        </MainLayout>
+      </CategoryContext.Provider>
     </NextUIProvider>
   );
 };
