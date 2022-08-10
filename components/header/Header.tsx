931f7e0e6f93a0fd8de91b0e2e623661818604b7
mengtongun <unmengtong@gmail.com>
2022-08-11 01:36:57 +0700

    feat: add category ui drawer and integrate

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,75 +1,62 @@
 /* This example requires Tailwind CSS v2.0+ */
 import { Popover, Transition } from '@headlessui/react';
-import { BookmarkAltIcon, CalendarIcon, MenuIcon, ShieldCheckIcon, SupportIcon, XIcon } from '@heroicons/react/outline';
+import {
+  BookmarkAltIcon,
+  CalendarIcon,
+  MenuAlt1Icon,
+  MenuIcon,
+  ShieldCheckIcon,
+  SupportIcon,
+  XIcon,
+} from '@heroicons/react/outline';
+import { Button } from '@nextui-org/react';
+import { Drawer, Menu } from 'antd';
+import { useAuth } from 'libs/hooks';
 import Image from 'next/image';
 import Link from 'next/link';
-import { Fragment } from 'react';
-// const solutions = [
-//   {
-//     name: 'Analytics',
-//     description: 'Get a better understanding of where your traffic is coming from.',
-//     href: '#',
-//     icon: ChartBarIcon,
-//   },
-//   {
-//     name: 'Engagement',
-//     description: 'Speak directly to your customers in a more meaningful way.',
-//     href: '#',
-//     icon: CursorClickIcon,
-//   },
-//   { name: 'Security', description: "Your customers' data will be safe and secure.", href: '#', icon: ShieldCheckIcon },
-//   {
-//     name: 'Integrations',
-//     description: "Connect with third-party tools that you're already using.",
-//     href: '#',
-//     icon: ViewGridIcon,
-//   },
-//   {
-//     name: 'Automations',
-//     description: 'Build strategic funnels that will drive your customers to convert',
-//     href: '#',
-//     icon: RefreshIcon,
-//   },
-// ];
-// const callsToAction = [
-//   { name: 'Watch Demo', href: '#', icon: PlayIcon },
-//   { name: 'Contact Sales', href: '#', icon: PhoneIcon },
-// ];
-const resources = [
-  {
-    name: 'Help Center',
-    description: 'Get all of your questions answered in our forums or contact support.',
-    href: '#',
-    icon: SupportIcon,
-  },
-  {
-    name: 'Guides',
-    description: 'Learn how to maximize our platform to get the most out of it.',
-    href: '#',
-    icon: BookmarkAltIcon,
-  },
-  {
-    name: 'About Us',
-    description: 'See what meet-ups and other events we might be planning near you.',
-    href: '#',
-    icon: CalendarIcon,
-  },
-  {
-    name: 'Data Policy',
-    description: 'Understand how we take your privacy seriously.',
-    href: '#',
-    icon: ShieldCheckIcon,
-  },
-];
-// const recentPosts = [
-//   { id: 1, name: 'Boost your conversion rate', href: '#' },
-//   { id: 2, name: 'How to use search engine optimization to drive traffic to your site', href: '#' },
-//   { id: 3, name: 'Improve your customer experience', href: '#' },
-// ];
+import { CategoryContext } from 'pages/_app';
+import { Fragment, useContext, useState } from 'react';
 
 export default function Header() {
+  const { isAuth } = useAuth();
+  const category = useContext(CategoryContext);
+  const [visible, setVisible] = useState(false);
+
+  const showDrawer = () => {
+    setVisible(true);
+  };
+
+  const onClose = () => {
+    setVisible(false);
+  };
+
   return (
     <Popover className="relative bg-white z-50">
+      <Drawer
+        title="Category"
+        headerStyle={{
+          textAlign: 'center',
+          fontSize: '2.5rem',
+        }}
+        placement="left"
+        closable={false}
+        onClose={onClose}
+        visible={visible}
+      >
+        <Menu mode="inline" defaultSelectedKeys={['1']}>
+          {category &&
+            category.map((item) => (
+              <Menu.Item key={item.id}>
+                <Link href={`/category/${item.id}`}>
+                  <a className="flex">
+                    <Image src={item.icon} objectFit="contain" width={50} height={25} />
+                    <p>{item.name}</p>
+                  </a>
+                </Link>
+              </Menu.Item>
+            ))}
+        </Menu>
+      </Drawer>
       <div className="max-w-7xl mx-auto px-4 sm:px-6">
         <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 lg:flex-1">
@@ -85,160 +72,28 @@ export default function Header() {
             </Popover.Button>
           </div>
           <Popover.Group as="nav" className="hidden md:flex space-x-10">
-            {/* <Popover className="relative">
-              {({ open }) => (
-                <>
-                  <Popover.Button
-                    className={classNames(
-                      open ? 'text-gray-900' : 'text-gray-500',
-                      'group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-                    )}
-                  >
-                    <span>Solutions</span>
-                    <ChevronDownIcon
-                      className={classNames(
-                        open ? 'text-gray-600' : 'text-gray-400',
-                        'ml-2 h-5 w-5 group-hover:text-gray-500'
-                      )}
-                      aria-hidden="true"
-                    />
-                  </Popover.Button>
-
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-200"
-                    enterFrom="opacity-0 translate-y-1"
-                    enterTo="opacity-100 translate-y-0"
-                    leave="transition ease-in duration-150"
-                    leaveFrom="opacity-100 translate-y-0"
-                    leaveTo="opacity-0 translate-y-1"
-                  >
-                    <Popover.Panel className="absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2">
-                      <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-                        <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-                          {solutions.map((item) => (
-                            <a
-                              key={item.name}
-                              href={item.href}
-                              className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50"
-                            >
-                              <item.icon className="flex-shrink-0 h-6 w-6 text-indigo-600" aria-hidden="true" />
-                              <div className="ml-4">
-                                <p className="text-base font-medium text-gray-900">{item.name}</p>
-                                <p className="mt-1 text-sm text-gray-500">{item.description}</p>
-                              </div>
-                            </a>
-                          ))}
-                        </div>
-                        <div className="px-5 py-5 bg-gray-50 space-y-6 sm:flex sm:space-y-0 sm:space-x-10 sm:px-8">
-                          {callsToAction.map((item) => (
-                            <div key={item.name} className="flow-root">
-                              <a
-                                href={item.href}
-                                className="-m-3 p-3 flex items-center rounded-md text-base font-medium text-gray-900 hover:bg-gray-100"
-                              >
-                                <item.icon className="flex-shrink-0 h-6 w-6 text-gray-400" aria-hidden="true" />
-                                <span className="ml-3">{item.name}</span>
-                              </a>
-                            </div>
-                          ))}
-                        </div>
-                      </div>
-                    </Popover.Panel>
-                  </Transition>
-                </>
-              )}
-            </Popover> */}
-
             <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
               Popular
             </a>
             <a href="#" className="text-base font-medium text-gray-500 hover:text-gray-900">
               Latest
             </a>
-
-            {/* <Popover className="relative">
-              {({ open }) => (
-                <>
-                  <Popover.Button
-                    className={classNames(
-                      open ? 'text-gray-900' : 'text-gray-500',
-                      'group bg-white rounded-md inline-flex items-center text-base font-medium hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-                    )}
-                  >
-                    <span>More</span>
-                    <ChevronDownIcon
-                      className={classNames(
-                        open ? 'text-gray-600' : 'text-gray-400',
-                        'ml-2 h-5 w-5 group-hover:text-gray-500'
-                      )}
-                      aria-hidden="true"
-                    />
-                  </Popover.Button>
-
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-200"
-                    enterFrom="opacity-0 translate-y-1"
-                    enterTo="opacity-100 translate-y-0"
-                    leave="transition ease-in duration-150"
-                    leaveFrom="opacity-100 translate-y-0"
-                    leaveTo="opacity-0 translate-y-1"
-                  >
-                    <Popover.Panel className="absolute z-10 left-1/2 transform -translate-x-1/2 mt-3 px-2 w-screen max-w-md sm:px-0">
-                      <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-                        <div className="relative grid gap-6 bg-white px-5 py-6 sm:gap-8 sm:p-8">
-                          {resources.map((item) => (
-                            <a
-                              key={item.name}
-                              href={item.href}
-                              className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-50"
-                            >
-                              <item.icon className="flex-shrink-0 h-6 w-6 text-indigo-600" aria-hidden="true" />
-                              <div className="ml-4">
-                                <p className="text-base font-medium text-gray-900">{item.name}</p>
-                                <p className="mt-1 text-sm text-gray-500">{item.description}</p>
-                              </div>
-                            </a>
-                          ))}
-                        </div>
-                        <div className="px-5 py-5 bg-gray-50 sm:px-8 sm:py-8">
-                          <div>
-                            <h3 className="text-sm tracking-wide font-medium text-gray-500 uppercase">Recent Posts</h3>
-                            <ul role="list" className="mt-4 space-y-4">
-                              {recentPosts.map((post) => (
-                                <li key={post.id} className="text-base truncate">
-                                  <a href={post.href} className="font-medium text-gray-900 hover:text-gray-700">
-                                    {post.name}
-                                  </a>
-                                </li>
-                              ))}
-                            </ul>
-                          </div>
-                          <div className="mt-5 text-sm">
-                            <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
-                              {' '}
-                              View all posts <span aria-hidden="true">&rarr;</span>
-                            </a>
-                          </div>
-                        </div>
-                      </div>
-                    </Popover.Panel>
-                  </Transition>
-                </>
-              )}
-            </Popover> */}
+            <a onClick={showDrawer} className="text-base font-medium text-gray-500 hover:text-gray-900">
+              All
+            </a>
           </Popover.Group>
-          <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-            <Link href="/signin">
-              <a className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">Sign in</a>
-            </Link>
-            <Link href="/signup">
-              <a className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
-                Sign up
-              </a>
-            </Link>
-          </div>
+          {!isAuth && (
+            <div className="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
+              <Link href="/signin">
+                <a className="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900">Sign in</a>
+              </Link>
+              <Link href="/signup">
+                <a className="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+                  Sign up
+                </a>
+              </Link>
+            </div>
+          )}
         </div>
       </div>
 
@@ -271,53 +126,38 @@ export default function Header() {
                   </Popover.Button>
                 </div>
               </div>
-              {/* <div className="mt-6">
-                <nav className="grid gap-y-8">
-                  {solutions.map((item) => (
-                    <a
-                      key={item.name}
-                      href={item.href}
-                      className="-m-3 p-3 flex items-center rounded-md hover:bg-gray-50"
-                    >
-                      <item.icon className="flex-shrink-0 h-6 w-6 text-indigo-600" aria-hidden="true" />
-                      <span className="ml-3 text-base font-medium text-gray-900">{item.name}</span>
-                    </a>
-                  ))}
-                </nav>
-              </div> */}
             </div>
             <div className="py-6 px-5 space-y-6">
               <div className="grid grid-cols-2 gap-y-4 gap-x-8">
-                <a href="#" className="text-base font-medium text-gray-900 hover:text-gray-700">
+                <a href="#" className="text-base font-medium text-gray-900 hover:text-gray-700  ">
                   Popular
                 </a>
 
                 <a href="#" className="text-base font-medium text-gray-900 hover:text-gray-700">
                   Latest
                 </a>
-                {resources.map((item) => (
-                  <a
-                    key={item.name}
-                    href={item.href}
-                    className="text-base font-medium text-gray-900 hover:text-gray-700"
-                  >
-                    {item.name}
-                  </a>
-                ))}
+                {category &&
+                  category.map((item) => (
+                    <Link href={`/category/${item.id}`} key={item.id}>
+                      <a className="text-base font-medium text-gray-500 hover:text-gray-900">{item.name}</a>
+                    </Link>
+                  ))}
               </div>
-              <div>
-                <Link href="/signup">
-                  <a className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
-                    Sign up
-                  </a>
-                </Link>
-                <p className="mt-6 text-center text-base font-medium text-gray-500">
-                  Existing customer?{' '}
-                  <Link href="/signin">
-                    <a className="text-indigo-600 hover:text-indigo-500">Sign in</a>
+              {!isAuth && (
+                <div>
+                  <Link href="/signup">
+                    <a className="w-full flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700">
+                      Sign up
+                    </a>
                   </Link>
-                </p>
-              </div>
+                  <p className="mt-6 text-center text-base font-medium text-gray-500">
+                    Existing customer?{' '}
+                    <Link href="/signin">
+                      <a className="text-indigo-600 hover:text-indigo-500">Sign in</a>
+                    </Link>
+                  </p>
+                </div>
+              )}
             </div>
           </div>
         </Popover.Panel>
