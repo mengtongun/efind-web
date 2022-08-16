981318d4e8b57a75309a3398fe87f0ece69f275d
mengtongun <unmengtong@gmail.com>
2022-08-16 21:58:17 +0700

    feat: add suggest list to Main Layout

--- layout/MainLayout.tsx
+++ layout/MainLayout.tsx
@@ -1,9 +1,12 @@
-import { Header } from '@/components';
+import { Header, SuggestList } from '@/components';
 
 const MainLayout = ({ children }) => {
   return (
     <div className="h-screen w-screen">
       <Header />
+      <div className="  w-full">
+        <SuggestList />
+      </div>
       {children}
     </div>
   );
