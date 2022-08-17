b94695042ce2c02ce73c440d0a4435b941fd4023
mengtongun <unmengtong@gmail.com>
2022-08-17 15:29:40 +0700

    feat: add popular page

---
+++ pages/popular/index.tsx
@@ -0,0 +1,28 @@
+import Body from 'layout/Body';
+import { supabase } from 'libs/supabase';
+import { GetServerSideProps } from 'next';
+
+const IndexPage = (props) => {
+  const { data } = props;
+  return (
+    <div className="w-full">
+      <Body stores={data} />
+    </div>
+  );
+};
+
+export default IndexPage;
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { data } = await supabase
+    .from('store')
+    .select('* ,category:c_id ( name )')
+    .order('view_count', { ascending: false })
+    .limit(30);
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
