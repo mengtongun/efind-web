68eaf4f7d1dc0c610417146773d21664ae75bf62
mengtongun <unmengtong@gmail.com>
2022-08-17 15:31:05 +0700

    chore: store card props

--- components/body/CardList.tsx
+++ components/body/CardList.tsx
@@ -1,15 +1,28 @@
 import { Grid, Text } from '@nextui-org/react';
 import { IStore } from 'interfaces';
 import { StoreCard } from '..';
+import { configResponsive, useResponsive } from 'ahooks';
+import { useEffect } from 'react';
 
-const CardList = ({ stores }: { stores: IStore[] }) =>
-  stores.length === 0 ? (
+configResponsive({
+  small: 0,
+  middle: 800,
+  large: 1200,
+});
+
+const CardList = ({ stores }: { stores: IStore[] }) => {
+  const responsive = useResponsive();
+  useEffect(() => {
+    return () => {};
+  }, []);
+
+  return stores.length === 0 ? (
     <Text className="text-center text-gray-500 text-2xl py-12">No stores found</Text>
   ) : (
-    <Grid.Container gap={2} justify="flex-start">
+    <Grid.Container gap={1} justify="flex-start">
       {stores.map((store, idx) => (
         <Grid key={idx} xs={6} sm={4} md={3} lg={3} xl={2}>
-          <StoreCard store={store} />
+          <StoreCard store={store} responsive={responsive} />
         </Grid>
       ))}
       {/* {[...Array(13)].map((_, i) => (
@@ -19,5 +32,6 @@ const CardList = ({ stores }: { stores: IStore[] }) =>
          ))} */}
     </Grid.Container>
   );
+};
 
 export default CardList;
