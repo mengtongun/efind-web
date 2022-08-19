a4d3b74450badc73d0ec5cb4974076561b419fb3
mengtongun <unmengtong@gmail.com>
2022-08-19 22:56:12 +0700

    chore: temp hide suggest list for sign up and in

--- layout/MainLayout.tsx
+++ layout/MainLayout.tsx
@@ -1,11 +1,14 @@
 import { Header, SuggestList } from '@/components';
+import { useRouter } from 'next/router';
 
 const MainLayout = ({ children }) => {
+  const router = useRouter();
+  const hideSuggestList = ['/signin', '/signup'].includes(router.pathname);
   return (
     <div className="h-screen w-screen">
       <Header />
 
-      <SuggestList />
+      {!hideSuggestList && <SuggestList />}
       {children}
     </div>
   );
