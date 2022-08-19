5ccfa7532c0012f90189dcc9ceb888bf0bd5a5d9
mengtongun <unmengtong@gmail.com>
2022-08-19 22:27:29 +0700

    feat: refactor  functions get data supabase client

---
+++ libs/providers/supabase-client.ts
@@ -0,0 +1,93 @@
+import { supabaseClient, User } from '@supabase/auth-helpers-nextjs';
+import { IStore, IUser, ICategory } from 'interfaces';
+
+const LIMIT_QUERY = 20;
+export const supabase = supabaseClient;
+
+// Example
+export const getActiveStores = async (): Promise<IStore[]> => {
+  const { data, error } = await supabase
+    .from('products')
+    .select('*, prices(*)')
+    .eq('active', true)
+    .eq('prices.active', true)
+    .order('metadata->index')
+    .order('unit_amount', { foreignTable: 'prices' });
+
+  if (error) {
+    console.log(error.message);
+    throw error;
+  }
+
+  return data || [];
+};
+
+export const updateUserName = async (user: User, name: string) => {
+  await supabase
+    .from<IUser>('user')
+    .update({
+      name: name,
+    })
+    .eq('id', user.id);
+};
+
+export const getStores = async (limit: number = LIMIT_QUERY): Promise<IStore[]> => {
+  const { data, error } = await supabase.from('store').select('* ,category:c_id ( name )').limit(limit);
+  console.log('getStores Error', error);
+  return data || [];
+};
+export const getStoreByPage = async (page: number = 1): Promise<IStore[]> => {
+  const { data, error } = await supabase
+    .from('store')
+    .select('* ,category:c_id ( name ) ')
+    .range(page, LIMIT_QUERY * page);
+  console.log('getStoreByPage Error', error);
+  return data || [];
+};
+
+export const getStoreByCategoryId = async (id: number | string | string[]): Promise<IStore[]> => {
+  const { data, error } = await supabase.from('store').select('* ,category:c_id ( name )').eq('c_id', id);
+  console.log('getStoreByCategoryId Error', error);
+  return data || [];
+};
+
+export const getLatestStore = async (): Promise<IStore[]> => {
+  const { data, error } = await supabase
+    .from('store')
+    .select('* ,category:c_id ( name )')
+    .order('id', { ascending: false })
+    .limit(30);
+
+  console.log('getLatestStore Error', error);
+  return data || [];
+};
+
+export const getPopularStore = async (): Promise<IStore[]> => {
+  const { data, error } = await supabase
+    .from('store')
+    .select('* ,category:c_id ( name )')
+    .order('view_count', { ascending: false })
+    .limit(30);
+
+  console.log('getPopularStore Error', error);
+
+  return data || [];
+};
+
+export const getStoreById = async (id: number | string): Promise<IStore[]> => {
+  const { data, error } = await supabase.from('store').select('* ,category:c_id ( name )').eq('id', id);
+  console.log('getStoreById Error', error);
+  return data || [];
+};
+
+export const getStoreByUserId = async (id: number | string): Promise<IStore[]> => {
+  const { data, error } = await supabase.from('store').select('* ,category:c_id ( name )').eq('o_id', id);
+  console.log('getStoreByUserId Error', error);
+  return data || [];
+};
+
+export const getCategories = async (): Promise<ICategory[]> => {
+  const { data, error } = await supabase.from('category').select('*');
+  console.log('getCategories Error', error);
+  return data || [];
+};
