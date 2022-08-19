2236d0ade75831564938dd30f0521b183a2a4752
mengtongun <unmengtong@gmail.com>
2022-08-19 22:24:50 +0700

    feat: add functions helper getUrl and toDate

--- libs/functions/index.ts
+++ libs/functions/index.ts
@@ -15,3 +15,19 @@ export const shuffle = (array) => {
     return array;
   }
 };
+
+export const getURL = () => {
+  const url =
+    process?.env?.URL && process.env.URL !== ''
+      ? process.env.URL
+      : process?.env?.VERCEL_URL && process.env.VERCEL_URL !== ''
+      ? process.env.VERCEL_URL
+      : 'http://localhost:3000';
+  return url.includes('http') ? url : `https://${url}`;
+};
+
+export const toDateTime = (secs: number) => {
+  const t = new Date('1970-01-01T00:30:00Z'); // Unix epoch start.
+  t.setSeconds(secs);
+  return t;
+};
