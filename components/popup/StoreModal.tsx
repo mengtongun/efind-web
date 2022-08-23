30b1906dc78992d9607f504c95cf2b271c4ec1ca
mengtongun <unmengtong@gmail.com>
2022-08-23 14:02:52 +0700

    feat: add rating mock to store

--- components/popup/StoreModal.tsx
+++ components/popup/StoreModal.tsx
@@ -1,4 +1,7 @@
+import { SaveFilled } from '@ant-design/icons';
 import { Modal, Button, Text } from '@nextui-org/react';
+import { useUser } from '@supabase/auth-helpers-react';
+import { notification, Rate } from 'antd';
 import { IStore } from 'interfaces';
 import Image from 'next/image';
 
@@ -13,6 +16,21 @@ declare type StoreModalPropsType = {
 
 const StoreModal = (props: StoreModalPropsType) => {
   const { bindings, setVisible, store } = props;
+  const { user } = useUser();
+  const onRate = (value) => {
+    console.log(value);
+    if (!user) {
+      notification.error({
+        message: 'Error',
+        description: 'Please login to rate',
+      });
+      return;
+    }
+    notification.success({
+      message: 'Success',
+      description: 'Thank you for your feedback',
+    });
+  };
   return (
     <div>
       <Modal scroll width="600px" aria-labelledby="modal-title" aria-describedby="modal-description" {...bindings}>
@@ -23,6 +41,7 @@ const StoreModal = (props: StoreModalPropsType) => {
         </Modal.Header>
         <Modal.Body>
           <Image src={store.logo} width={200} height={200} objectFit="contain" />
+          <Rate defaultValue={store.rating || 2} onChange={onRate} />
           <Text id="modal-description">{store.description}</Text>
         </Modal.Body>
         <Modal.Footer>
