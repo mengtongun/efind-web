2c260543778766e7244772415d2c501c71322ff6
mengtongun <unmengtong@gmail.com>
2022-08-27 16:36:38 +0700

    feat: add default seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,11 +9,38 @@ import '../styles/globals.css';
 export const CategoriesContext = createContext<ICategory[]>(null);
 import { UserProvider } from '@supabase/auth-helpers-react';
 import { supabaseClient } from '@supabase/auth-helpers-nextjs';
+import { NextSeo } from 'next-seo';
 
 const MyApp = ({ Component, pageProps }) => {
   const { categories } = useCategories();
+
   return (
     <UserProvider supabaseClient={supabaseClient}>
+      <NextSeo
+        noindex={true}
+        defaultTitle="eFind"
+        description="eFind is a platform that helps you find the best stores in your area. You can find the best stores in your area by searching for them or by browsing through the categories. You can also find the best stores in your area by searching for them or by browsing through the categories. You can also find the best stores in your area by searching for them or by browsing through the categories."
+        openGraph={{
+          type: 'website',
+          url: window.location.href,
+          title: 'Popular',
+          description: 'eFind | Find your trusted online store',
+          images: [
+            {
+              url: 'https://teiiihfrnoybdttheiwg.supabase.co/storage/v1/object/public/logos/efind_official.png',
+              width: 800,
+              height: 600,
+              alt: 'eFind Web Logo',
+            },
+            {
+              url: 'https://teiiihfrnoybdttheiwg.supabase.co/storage/v1/object/public/logos/efind_logo.png',
+              width: 800,
+              height: 600,
+              alt: 'eFind Admin Logo',
+            },
+          ],
+        }}
+      />
       <NextUIProvider>
         <CategoriesContext.Provider value={categories}>
           <MainLayout>
