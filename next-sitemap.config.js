00e06950dc2af9b5f3e81989ac409223245311ce
mengtongun <unmengtong@gmail.com>
2022-08-28 11:15:04 +0700

    chore: update sitemap config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,4 +3,21 @@
 module.exports = {
   siteUrl: process.env.SITE_URL || 'https://efind.vercel.app',
   generateRobotsTxt: true,
+  changefreq: 'weekly',
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      },
+      {
+        userAgent: 'test-bot',
+        allow: ['/signin', '/signup'],
+      },
+      {
+        userAgent: 'black-listed-bot',
+        disallow: ['/signin', '/signup'],
+      },
+    ],
+  },
 };
