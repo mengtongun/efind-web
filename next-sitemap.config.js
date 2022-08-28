b4a472f21eda0d751973b8f92611a303db5d25ce
mengtongun <unmengtong@gmail.com>
2022-08-28 11:24:11 +0700

    chore: mod config

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,20 +4,21 @@ module.exports = {
   siteUrl: process.env.SITE_URL || 'https://efind.vercel.app',
   generateRobotsTxt: true,
   changefreq: 'weekly',
+  exclude: ['/sitemap.xml.v1'],
   robotsTxtOptions: {
     policies: [
       {
         userAgent: '*',
         allow: '/',
       },
-      {
-        userAgent: 'test-bot',
-        allow: ['/signin', '/signup'],
-      },
-      {
-        userAgent: 'black-listed-bot',
-        disallow: ['/signin', '/signup'],
-      },
+      // {
+      //   userAgent: 'test-bot',
+      //   allow: ['/signin', '/signup'],
+      // },
+      // {
+      //   userAgent: 'black-listed-bot',
+      //   disallow: ['/signin', '/signup'],
+      // },
     ],
   },
 };
